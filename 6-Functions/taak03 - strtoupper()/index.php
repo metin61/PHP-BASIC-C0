ce19da6ce62fd3c018983dd552d396233a4fde15
metin celik <metin.celik@student.rocva.nl>
2020-11-25 11:52:47 +0100

    strlen functie

---
+++ 6-Functions/taak03 - strtoupper()/index.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function telAlleWoorden(){
+            $auto ="Mijn blauwe auto is een mooie snelle wagen";
+            strlen($auto);
+            
+            $lengteZin = $auto;
+            echo $lengteZin;
+
+         }
+         telAlleWoorden();
+
+    ?>
+</body>
+</html>
