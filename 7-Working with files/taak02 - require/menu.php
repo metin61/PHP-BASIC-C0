3a0bd83c929608f82df5b2da570eba44ca4d83e7
metin celik <metin.celik@student.rocva.nl>
2020-11-25 13:45:22 +0100

    require

---
+++ 7-Working with files/taak02 - require/menu.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+$item = "<li>menu-item 1</li>";
+$item .= "<li>menu-item 2</li>";
+$item .= "<li>menu-item 3</li>";
+echo $item;
+ ?>
+
+</body>
+</html>
