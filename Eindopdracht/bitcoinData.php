22f44fb4b61209acecd70f80f60d6717007641e8
metin celik <metin.celik@student.rocva.nl>
2020-12-02 13:40:26 +0100

    eindopdracht

--- Eindopdracht/bitcoinData.php
+++ Eindopdracht/bitcoinData.php
@@ -18,6 +18,7 @@
 /* 
  * Opdracht 2: Zet hier de $buttons array neer 
  */
+$buttons = array (9.99,50,1000);
 
 
 /******************************************* OPDRACHT 2 doe je hierboven *******************************************/
@@ -41,9 +42,11 @@ function calculateBitcoinAmount()
     }
 
     //SCHRIJF JE CODE.
+    $bitcoinPrice = getBitcoinPrice();
+    $bitcoinAmount = $euro / $bitcoinPrice;
+    return $bitcoinAmount;
 
-
-
+    
 }
 /******************************************* OPDRACHT 3 doe je hierboven *******************************************/
 /*******************************************************************************************************************/
@@ -79,6 +82,8 @@ if (isset($_GET['euro'])) {
      * Schrijf hieronder de code om de gekochte bitcoin aan de array $transactions toe te voegen.
      * Je voegt een bitcoinprijs toe, de prijs in euro's en het aantal gekochte bitcoins.
      */
+    array_push($transactions, [getBitcoinPrice(),$euro, calculateBitcoinAmount()]);
+    
 }
 
 
